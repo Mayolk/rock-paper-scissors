69fbdd271538c4058ef04f9eec5f60cc1a814f22
Mayolk <semir_begagic@yahoo.com>
2021-07-15 16:01:00 +0200

    Created function that starts a round and records results
    Function starts rounds
    
    Function records results

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ function computerPlay() {
 // Take player input and computer input and simulate a round
 function playRound (playerSelection, computerSelection) {
   let computer = computerSelection;
-  console.log('Computer:', computer);
+  console.log(computer);
   let player = playerSelection.toLowerCase();
-  console.log('Player:', player);
+
   switch (computer) {
     case 'rock':
       switch (player) {
         case 'rock':
         console.log("It's a draw.");
-        break;
+        return 'draw';
         case 'paper':
-        console.log("You win.");
-        break;
-        case 'scissor':
-        console.log("You lose.");
-        break;
+        console.log("You win. Paper beats rock.");
+        return 'win';
+        case 'scissors':
+        console.log("You lose. Rock beats scissors.");
+        return 'lose';
         default:
         console.log('Something went wrong.');
       }
@@ -35,14 +35,14 @@ function playRound (playerSelection, computerSelection) {
     case 'paper':
       switch (player) {
         case 'rock':
-        console.log("You lose.");
-        break;
+        console.log("You lose. Paper beats rock.");
+        return 'lose';
         case 'paper':
         console.log("It's a draw.");
-        break;
-        case 'scissor':
-        console.log("You win.");
-        break;
+        return 'draw';
+        case 'scissors':
+        console.log("You win. Scissors beat paper.");
+        return 'win';
         default:
         console.log('Something went wrong.');
       }
@@ -50,14 +50,14 @@ function playRound (playerSelection, computerSelection) {
     case 'scissors':
       switch (player) {
         case 'rock':
-        console.log("You win.");
-        break;
+        console.log("You win. Rock beats scissors.");
+        return 'win';
         case 'paper':
-        console.log("You lose.");
-        break;
-        case 'scissor':
+        console.log("You lose. Scissors beat paper.");
+        return 'lose';
+        case 'scissors':
         console.log("It's a draw.");
-        break;
+        return 'draw';
         default:
         console.log('Something went wrong.');
       }
@@ -67,5 +67,46 @@ function playRound (playerSelection, computerSelection) {
   }
 
 }
+// Take input from the user and playRound.
+// Record results
+function game () {
+  let playerCount = 0;
+  let computerCount = 0;
 
-playRound('PapeR', computerPlay());
+  let lastRound = playRound(prompt('Round 1. Rock, paper or scissors','') , computerPlay());
+  if ( lastRound === 'win') {
+    playerCount++;
+  } else if (lastRound === 'lose')  {
+    computerCount++;
+  }
+  lastRound = playRound(prompt('Round 2. Rock, paper or scissors','') , computerPlay());
+  if ( lastRound === 'win') {
+    playerCount++;
+  } else if (lastRound === 'lose')  {
+    computerCount++;
+  }
+  lastRound = playRound(prompt('Round 3. Rock, paper or scissors','') , computerPlay());
+  if ( lastRound === 'win') {
+    playerCount++;
+  } else if (lastRound === 'lose')  {
+    computerCount++;
+  }
+
+  lastRound = playRound(prompt('Round 4. Rock, paper or scissors','') , computerPlay());
+  if ( lastRound === 'win') {
+    playerCount++;
+  } else if (lastRound === 'lose')  {
+    computerCount++;
+  }
+
+  lastRound = playRound(prompt('Round 5. Rock, paper or scissors','') , computerPlay());
+  if ( lastRound === 'win') {
+    playerCount++;
+  } else if (lastRound === 'lose')  {
+    computerCount++;
+  }
+
+  console.log(`Final result: You:${playerCount} - Comp:${computerCount}`);
+}
+
+game();
