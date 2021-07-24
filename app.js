b46d53df4b421392369c77191fe48bd5807c98ec
Mayolk <semir_begagic@yahoo.com>
2021-07-24 13:10:53 +0200

    Implemented one round play with UI

--- app.js
+++ app.js
@@ -11,55 +11,54 @@ function computerPlay() {
 }
 
 // Take player input and computer input and simulate a round
-function playRound (playerSelection, computerSelection) {
-  let computer = computerSelection;
-  console.log(computer);
-  let player = playerSelection.toLowerCase();
-
+function playRound () {
+  let computer = computerPlay();
+  let player = this.textContent.toLowerCase();
+  const lastRound = document.querySelector('#last-round');
   switch (computer) {
     case 'rock':
       switch (player) {
         case 'rock':
-        console.log("It's a draw.");
+        lastRound.textContent = "It's a draw.";
         return 'draw';
         case 'paper':
-        console.log("You win. Paper beats rock.");
+        lastRound.textContent = "You win. Paper beats rock.";
         return 'win';
         case 'scissors':
-        console.log("You lose. Rock beats scissors.");
+        lastRound.textContent = "You lose. Rock beats scissors.";
         return 'lose';
         default:
-        console.log('Something went wrong.');
+        lastRound.textContent = 'Something went wrong.';
       }
     break;
     case 'paper':
       switch (player) {
         case 'rock':
-        console.log("You lose. Paper beats rock.");
+        lastRound.textContent = "You lose. Paper beats rock.";
         return 'lose';
         case 'paper':
-        console.log("It's a draw.");
+        lastRound.textContent = "It's a draw.";
         return 'draw';
         case 'scissors':
-        console.log("You win. Scissors beat paper.");
+        lastRound.textContent = "You win. Scissors beat paper.";
         return 'win';
         default:
-        console.log('Something went wrong.');
+        lastRound.textContent = 'Something went wrong.';
       }
     break;
     case 'scissors':
       switch (player) {
         case 'rock':
-        console.log("You win. Rock beats scissors.");
+        lastRound.textContent = "You win. Rock beats scissors.";
         return 'win';
         case 'paper':
-        console.log("You lose. Scissors beat paper.");
+        lastRound.textContent = "You lose. Scissors beat paper.";
         return 'lose';
         case 'scissors':
-        console.log("It's a draw.");
+        lastRound.textContent = "It's a draw.";
         return 'draw';
         default:
-        console.log('Something went wrong.');
+        lastRound.textContent = 'Something went wrong.';
       }
     break;
     default:
@@ -74,20 +73,10 @@ function game () {
   let playerCount = 0;
   let computerCount = 0;
 
-
-  for (i = 1; i <= 5; i++) {
-
-    let lastRound = playRound(prompt(`Round ${i}. Rock, paper or scissors`,''), computerPlay());
-
-    if ( lastRound === 'win') {
-      playerCount++;
-    } else if (lastRound === 'lose') {
-      computerCount++;
-    }
-  }
+  const moves = document.querySelectorAll('.move');
+  moves.forEach( move => move.addEventListener('mouseup', playRound));
 
 
-  console.log(`Final result: You:${playerCount} - Comp:${computerCount}`);
 }
 
 game();
